8b3acfbd5a7cb71aac86c80b45f5af65ab97751b
孤燈 <weiyang.jwy@alibaba-inc.com>
2021-06-16 11:24:58 +0800

    [feature] init playground

---
+++ app/src/main/java/com/bbbond/playground/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.bbbond.playground
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.bbbond.playground.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
